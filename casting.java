8a311a70a6f9e6b295a4ab1eb12fe42a0c9ef086
Pedro Cunegundes <bangue062@gmail.com>
2023-10-04 10:05:15 -0300

    Primeiro commit

---
+++ casting.java
@@ -0,0 +1,8 @@
+public class casting {
+  public static void main(String[] args) {
+    int INTEGER = 9;
+    Double DOUBLE = new Double(INTEGER);
+    System.out.println(INTEGER);
+    System.out.println(DOUBLE);
+  }
+}
